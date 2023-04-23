71f1854892953278995d4c91a184063bb9c0c90d
Abenitesl2 <113810203+Abenitesl2@users.noreply.github.com>
2023-04-22 21:22:23 -0500

    feat: Crear la clase parkingServices. Implemento los metodos getAllCars y addCar

--- API-PARKING/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ API-PARKING/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -1,4 +1,33 @@
 package com.parking.apiparking.services;
 
+import com.parking.apiparking.entities.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingService {
+    private List<Car> parkingLot;
+
+
+    public ParkingService(){
+
+        this.parkingLot = new ArrayList<>();
+
+
+    }
+
+    public List<Car> getAllCars(){
+
+        return this.parkingLot;
+
+    }
+
+    public void addCar(Car car){
+
+        this.parkingLot.add(car);
+
+
+    }
+
+
 }
