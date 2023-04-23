37d7d68121c6066c6375238099a58f2b07070927
Abenitesl2 <113810203+Abenitesl2@users.noreply.github.com>
2023-04-22 21:44:22 -0500

    feat: Crear la clase ParkingController- implementando los metodos de getAllCars y addCar

--- API-PARKING/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ API-PARKING/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -1,4 +1,46 @@
 package com.parking.apiparking.controller;
 
+import com.parking.apiparking.services.ParkingService;
+import com.parking.apiparking.entities.Car;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking") //parking => url
 public class ParkingController {
+
+    //Inyeccion de Dependecias => instancia => new ParkingService();
+
+   private final  ParkingService parkingService;
+
+   public ParkingController(ParkingService parkingService){
+
+       this.parkingService = parkingService;
+
+   }
+
+    //http://dominio/parking/cars
+@GetMapping("/cars")
+        public ResponseEntity<List<Car>> getAllCars(){
+
+            return ResponseEntity.ok(this.parkingService.getAllCars());
+
+    }
+
+    @GetMapping("/cars")
+    public ResponseEntity<Car> addCar(Car car){
+
+      this.parkingService.addCar(car);
+
+      return new ResponseEntity<>(car, HttpStatus.CREATED);
+
+    }
+
+
+
 }
